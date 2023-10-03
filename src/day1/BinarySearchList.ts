d21806d4c9d39a203bdd33ac71219080f6b78ef1
Hunter Kamerman <devkamerman@gmail.com>
2023-10-03 12:44:15 -0400

    tests fail

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,12 +1,24 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-   
     let lo = 0;
-    let hi = haystack.length;
-   
-    do{
-        const m = Math.floor(lo + (hi - lo)/2);
-        const v = haystac[m];
-        while
-    }
+    let hi = haystack.length - 1;
 
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo <= hi); // Keep the do...while loop condition the same
+
+    return false;
 }
+
+
+
+
+
