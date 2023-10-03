a4d8d8e3eacc80ea4e11178a537758cb767518dc
Hunter Kamerman <devkamerman@gmail.com>
2023-10-03 14:13:12 -0400

    passes one might fail cuz differernt file

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,6 +1,6 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
-    let hi = haystack.length - 1;
+    let hi = haystack.length;
 
     do {
         const m = Math.floor(lo + (hi - lo) / 2);
@@ -13,7 +13,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
         } else {
             lo = m + 1;
         }
-    } while (lo <= hi); // Keep the do...while loop condition the same
+    } while (lo < hi); // Keep the do...while loop condition the same
 
     return false;
 }
