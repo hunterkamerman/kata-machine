88a88799f7cd4203ccf95c38b200286379e25952
Hunter Kamerman <devkamerman@gmail.com>
2023-10-03 12:26:51 -0400

    bi

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,12 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+   
+    let lo = 0;
+    let hi = haystack.length;
+   
+    do{
+        const m = Math.floor(lo + (hi - lo)/2);
+        const v = haystac[m];
+        while
+    }
 
 }
